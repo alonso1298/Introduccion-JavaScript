af538cfb337948e5aa44105be8063d473d0c24eb
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-06-22 04:24:02 -0600

    Se agregan los archivos y varibles

---
+++ js/01_variables.js
@@ -0,0 +1,19 @@
+// Variables 
+
+var producto = 'Audifonos Gamer'; //Iniciar variable y se asigna un valor 
+
+var disponible; //Inicia la variable sin valor
+
+producto = true; // Reasignando el valor de la variable 
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; // Se inicializan multiples variables
+
+// var 1disponible; las variables no pueden iniciar con numeros
+// var -disponible; las variables no pueden iniciar con guiones
+var _disponible; //Si pueden iniciar con guion bajo
+
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; //underscore
+
