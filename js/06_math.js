65e715980d2ebc156fd2866cd5ce85d76ae93624
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-06-24 17:47:43 -0600

    Se agrega objeto Math

---
+++ js/06_math.js
@@ -0,0 +1,17 @@
+// Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.6); //Redondea despues de .6 hacia arriba y .5 hacia abajo
+resultado = Math.ceil(2.1); //Ceil redondea hacia arriba
+resultado = Math.floor(2.9); //floor redondea hacia abajo
+resultado = Math.sqrt(144); //Raiz cuadrada
+resultado = Math.abs(-50); //Convierte un numero negativo a positivo
+resultado = Math.min(3, 5, 8, 9); //Nos arroja el minimo numero de un listado
+resultado = Math.max(3, 5, 8, 9); //Nos arroja el maximo numero de un listado
+resultado = Math.random(); //Se genera un numero aleatorio entre 0 a 1
+resultado = Math.floor( Math.random() * 30) //mezclando una funcion con otra y una multiplicacion podemos tener valores enteros en un rango 
+
+
+console.log(resultado);
