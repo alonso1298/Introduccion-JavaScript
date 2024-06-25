11fce4d6fbf11f05ae5b9d528b78defe131ebd5c
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-06-24 18:09:17 -0600

    Se agrega orden de las operaciones

---
+++ js/07_ordenOperaciones.js
@@ -0,0 +1,25 @@
+// Orden de las Operaciones
+
+let resultado;
+
+//Sigue el orden jerarquico de las operaciones
+resultado = 20 + 30 * 2; 
+//Para controlar esto podemos agragar parentesis
+resultado = (20 + 30) * 2; 
+
+console.log(resultado);
+
+// Incrementos 
+
+let puntaje = 10;
+
+// Incremento en 1 ++
+puntaje++; //10
+++puntaje //12
+
+puntaje-- //11
+
+// Incremento en num 
+puntaje += 10; //Incrementa de 10 en 10
+
+console.log(puntaje);
