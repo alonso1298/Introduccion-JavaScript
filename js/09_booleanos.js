f5c9e70014337415e8ca74bc7c06b1242a8f05c5
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-06-25 22:14:54 -0600

    Se agregan booleanos

---
+++ js/09_booleanos.js
@@ -0,0 +1,13 @@
+// Booleanos
+
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = 'true'
+
+console.log(typeof boolean1);
+console.log(typeof boolean2);
+console.log(typeof boolean3);
+
+// Con constructor 
+const boolean4 = new Boolean(true);
+console.log(boolean4);
