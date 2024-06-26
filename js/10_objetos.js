07f246d999bbb512666a0253a4bedadc109a9ae9
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-06-25 22:29:37 -0600

    Se agregan objetos

---
+++ js/10_objetos.js
@@ -0,0 +1,19 @@
+// Objetos
+
+const nombreProducto = "Monitor 20 Pulgadas";
+const precio = 300;
+const disponible = true;
+
+const producto = {
+    nombreProducto : "Monitor 20 Pulgadas", // En ves de poner = se pone : 
+    precio : 300,
+    disponible : true,
+}
+
+console.log(producto);
+console.log(producto.precio); //Para acceder a las propiedades es con un punto seguido del nombre de la propiedad
+console.log(producto.disponible);
+console.log(producto.nombreProducto);
+
+console.log(producto["precio"]); //Este metodo no es tan usado 
+
