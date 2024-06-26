590289acbfa5a4824ef94025a0b2d464e5938261
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-06-25 22:05:48 -0600

    Se agrega template strings

---
+++ js/08_concatenacion.js
@@ -0,0 +1,9 @@
+const nombre = 'Alonso';
+const email = 'alonso@gmail.com';
+
+// Concatenación
+console.log(nombre + email); // El signo de + entre 2 str, este los junta
+console.log(nombre + " " + email);  // Al poner el espacio en medio este lo concatena
+
+// Template Strings - String Literals
+console.log(`Nombre cliente: ${nombre} Email: ${email}`); // Solo se puede utilizar con ``
