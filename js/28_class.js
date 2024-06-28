8abbc489ee45cef7672518621e7e3075f9ac6f92
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-06-28 03:32:26 -0600

    Se agrega Clases

--- js/28_class.js
+++ js/28_class.js
@@ -5,8 +5,15 @@ class Producto{
         this.nombre = nombre;
         this.precio = precio;
     }
+    formatearProducto() {
+        return `El producto: ${this.nombre} tiene un precio de: ${this.precio}`
+    }
+    obtenerPrecio(){
+        return `El precio es: ${this.precio}`
+    }
 }
 
 const producto = new Producto('iPhone', 12000 )
-console.log(producto.nombre)
+console.log(producto.formatearProducto())
+console.log(producto.obtenerPrecio())
 
