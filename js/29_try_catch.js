5b4e2baf1d1d874a5213f14bfd343dc412b29109
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2024-06-28 04:12:02 -0600

    Se agrega Try Catch

---
+++ js/29_try_catch.js
@@ -0,0 +1,14 @@
+// Try - Catch
+
+const numero1 = 10;
+const numero3 = 15;
+
+console.log(numero1);
+
+try { //Try intentara hacer algo
+    console.log(numero2);
+} catch (error) { //Si no caera en el catch
+    console.log(error)
+}
+
+console.log(numero3);
